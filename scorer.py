634449ddad12ac6e54ca5180b9b029f068012112
AaronVillegas5 <akvilleg@uci.edu>
2026-02-28 12:36:45 -0800

    Added backend/scorer requirements.txt and scorer updates, will test on Render

--- scorer.py
+++ scorer.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sqlalchemy import null
-from main import load_events, load_routes, load_users, load_lodging
+from db import load_events, load_routes, load_users, load_lodging
 from datetime import datetime
 
 def nights(start_ts, end_ts):
